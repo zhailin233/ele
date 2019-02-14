d548a3d5b2d3e501eece158ad052c83c4a9d10f7
翟林 <18731810122@163.com>
2019-02-14 13:19:15 +0800

    重置样式

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,9 +1,22 @@
 // scaffold
 <template>
-  <div>
-    <div>
-      <div>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
         <img :src="seller.avatar" alt="">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div class="supports" v-if="seller.supports">
+          <span class="ico" :class="calssMap[seller.supports[0].type]"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -17,17 +30,9 @@ export default {
   props: {
     seller: {}, 
   },
-  // created() {
-  //   console.log(this.seller,1)
-  // },  
-  // mounted() {
-  //   console.log(this.seller,2)
-  // },
-  // watch: {
-  //   seller: function(){
-  //     console.log(this.seller,2)
-  //   }  
-  // },
+  created() {
+    this.calssMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
   methods: {
 
   }
@@ -35,6 +40,44 @@ export default {
 
 </script>
 
-<style>
-
+<style lang='stylus' rel='stylesheet/stylus'>
+  @import '../../common/stylus/mixin.styl';
+  .header
+    color #fff
+    position relative
+    overflow hidden
+    background rgba(7,17,27,0.5)
+    .content-wrapper
+      position relative
+      padding 24px 12px 18px 24px
+      font-size 0
+      .avatar
+        display inline-block
+        img 
+          width 64px
+          border-radius 2px
+      .content
+        margin-left 16px
+        display inline-block
+        vertical-align top 
+        font-size 12px
+        .title
+          margin 2px 0 8px 0
+          .brand
+            display inline-block
+            vertical-align top
+            width 30px
+            height 18px
+            bg-image('brand')
+            background-size 30px 18px
+            background-repeat no-repeat
+          .name
+            margin-left 16px
+            font-size 16px
+            line-height 18px
+            font-weight bold
+        .description
+            font-size 12px
+            margin-bottom 10px
+            line-height 12px
 </style>
