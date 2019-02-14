7ddeed248991fcf91dc7c30434d3afa5c999478f
翟林 <18731810122@163.com>
2019-02-14 17:52:24 +0800

    完成header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -14,27 +14,86 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div class="supports" v-if="seller.supports">
-          <span class="ico" :class="calssMap[seller.supports[0].type]"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div @click="showDetail" class="support-count">
+        <span v-if="seller.supports" class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
+      
     </div>
+    <div @click="showDetail" class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" alt="">
+    </div>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size='48' :score='seller.score'></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul class="supports" v-if="seller.supports">
+              <li class="support-item" v-for="(item, index) in seller.supports" :key="item.id">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import star from '../stars/star.vue'
 export default {
   components: {
-    // star
+    star
+  },
+  data() {
+    return {
+      detailShow: false
+    }
   },
   props: {
     seller: {}, 
   },
   created() {
-    this.calssMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   },
   methods: {
-
+    showDetail() {
+      this.detailShow = true
+    },
+    hideDetail() {
+      this.detailShow = false
+    }
   }
 }
 
@@ -77,7 +136,172 @@ export default {
             line-height 18px
             font-weight bold
         .description
-            font-size 12px
-            margin-bottom 10px
+          font-size 12px
+          margin-bottom 10px
+          line-height 12px
+        .supports
+          .text
             line-height 12px
+            font-size 12px
+          .icon
+            display inline-block
+            vertical-align top
+            width 12px
+            height 12px
+            margin-right 4px
+            background-repeat no-repeat
+            background-size 12px 12px
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+      .support-count
+        position absolute
+        right 12px
+        bottom 14px
+        padding 0 8px
+        height 24px
+        line-height 24px
+        border-radius 14px
+        background rgba(0,0,0,0.2)
+        text-align center
+        .count
+          vertical-align top
+          font-size 10px
+        .icon-keyboard_arrow_right
+          line-height 24px
+          margin-right 2px
+          font-size 10px   
+    .bulletin-wrapper 
+      height 28px
+      padding 0 22px 0 12px
+      line-height 28px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      position relative
+      background rgba(7,17,27,0.2)
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 6px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        font-size 10px
+        margin-left 4px
+        vertical-align top
+      .icon-keyboard_arrow_right
+        position :absolute
+        font-size: 10px
+        right: 12px
+        top:8px
+    .background
+      position absolute
+      top 0
+      left 0
+      z-index -1
+      width 100%
+      height 100%
+      filter blur(10px)
+      img 
+        width 100%
+    .detail
+      position fixed
+      z-index 100
+      top 0
+      left 0
+      width 100%
+      height 100%
+      overflow auto
+      background rgba(7,17,27,0.8)
+      backdrop-fillter blur(10px)
+      .detail-wrapper
+        min-height 100%
+        width 100%
+        .detail-main
+          margin-top 64px
+          padding-bottom 64px
+          .name
+            line-height 16px
+            font-size 16px
+            font-weight 700
+            text-align center
+          .star-wrapper
+            margin-top 18px
+            padding 2px 0
+            text-align center 
+          .title
+            display flex
+            width 80%
+            margin 28px auto 24px
+            .line
+              flex 1
+              position relative
+              top -6px
+              border-bottom 1px solid rgba(255,255,255,0.2)
+            .text
+              padding 0 12px
+              font-size 14px
+              font-weight 700
+          .supports
+            width 80%
+            margin 0 auto
+            .support-item
+              padding 0 12px
+              margin-bottom 12px
+              &last-child
+                margin-bottom 0
+              .icon
+                display inline-block
+                width 16px
+                height 16px
+                vertical-align top
+                margin-right 6px
+                background-size 16px 16px
+                background-repeat no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                font-size 12px
+                line-height 16px
+          .bulletin
+            width 80%
+            margin 0 auto
+            .content
+              padding 0 12px
+              line-height 24px
+              font-size 12px
+  .detail-close
+    position relative
+    width 32px
+    height 32px
+    margin -60px auto 
+    clear both
+    font-size 32px
+    color rgba(255,255,255,0.5)
+
+  .fade-enter-active, .fade-leave-active
+    opacity 1
+    transition all .5s
+  .fade-enter, .fade-leave-active
+    opacity 0    
+
 </style>
---
+++ src/components/stars/star.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="itemClass in itemClasses" :key='itemClass.id' :class="itemClass" class="star-item"></span>
+  </div>
+</template>
+
+<script>
+const LENGTH = 5;
+const CLS_ON = 'on';
+const CLS_HALF = 'half';
+const CLS_OFF = 'off'
+export default {
+  props: {
+    size: {},
+    score: {}
+  },
+  computed: {
+    starType() {
+      return 'star-'+this.size
+    },
+    itemClasses() {
+      let result = [];
+      let score = Math.floor(this.score*20)/2
+    }
+  }
+} 
+</script>
+
+<style>
+
+</style>
