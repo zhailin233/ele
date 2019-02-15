1dec39c25dbb02325e518192587f990b202367c7
翟林 <18731810122@163.com>
2019-02-15 17:04:35 +0800

    完成goods

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,15 +1,204 @@
 <template>
-  <div>
-    food
-  </div>
+  <transition name='slide'>
+    <div class="food" v-show="showFlag" ref="food">
+      food
+    </div>  
+  </transition>
 </template>
 
 <script>
 export default {
+  props: {
+    food: {
+      type: Object
+    }
+  },
+  components: {
 
+  },
+  data() {
+    return {
+      showFlag: false,
+      
+    }
+  },
+  methods: {
+    show() {
+      this.showFlag = true;
+      this.$nextTick(()=>{
+        if(!this.scroll){
+          this.scroll = new BScroll(this.$refs.food,{
+            click:true
+          })
+        }else{
+          this.scroll.refresh();
+        }
+      });
+    },
+    hide() {
+      this.showFlag = false
+    }
+  }
 }
 </script>
 
-<style>
+<style lang='stylus'>
+  @import "../../common/stylus/mixin.styl"
+  .food
+    position :fixed
+    left:0
+    top:0
+    bottom: 48px
+    z-index:30
+    width :100%
+    background :#fff
+    .image-header
+      position :relative
+      width :100%
+      height :0
+      padding-top :100%
+      img
+        position :absolute
+        top:0
+        left:0
+        width :100%
+        height: 100%
+      .back
+        position :absolute
+        top: 10px
+        left:0
+        .icon-arrow_lift
+          display :block
+          padding :10px
+          font-size :20px
+          color :#fff
 
+    .content
+      padding :18px
+      position :relative
+      .title
+        line-height :14px
+        margin-bottom: 8px
+        font-size :14px
+        font-weight :700
+        color:rgb(7,17,27)
+      .detail
+        margin-bottom :18px
+        line-height :10px
+        height :10px
+        font-size :0
+        .sell-count,.rating
+          font-size :10px
+          color :rgb(147,153,159)
+        .sell-count
+          margin-right :12px
+      .price
+        line-height :24px
+        .now
+          margin-right: 18px
+          font-size :14px
+          font-weight :700
+          color:rgb(240,20,20)
+          .money-icon
+            font-size :10px
+        .old
+          text-decoration :line-through
+          font-size :10px
+          font-weight :700
+          margin-left -15px
+          color :rgb(147,153,159)
+      .cartcontrol-wrapper
+        position :absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position :absolute
+        right:18px
+        bottom: 18px
+        z-index: 10
+        height :24px
+        line-height :24px
+        padding :0 12px
+        box-sizing :border-box
+        font-size :10px
+        border-radius :12px
+        color :#fff
+        background :rgb(0,160,220)
+    .info
+      padding: 18px
+      .title
+        line-height :14px
+        margin-bottom :6px
+        font-size :14px
+        color:rgb(7,17,27)
+      .text
+        line-height :24px
+        padding:0 8px
+        font-size:12px
+        color:rgb(77,85,93)
+    .rating
+      padding-top :18px
+      padding-bottom :48px
+      .title
+        margin-left :18px
+        line-height :14px
+        font-size :14px
+        color:rgb(7,17,27)
+      .rating-wrapper
+        padding:0 18px
+        .rating-item
+          position :relative
+          padding: 16px 0
+          border-1px(rgba(7,17,27,0.1))
+          .user
+            position :absolute
+            right:0
+            top: 16px
+            line-height :12px
+            font-size :0
+            .name
+              display :inline-block
+              vertical-align :top
+              margin-right :6px
+              font-size :10px
+              color:rgb(147,153,159)
+            .avatar
+              border-radius :50%
+          .time
+            margin-bottom :6px
+            line-height :12px
+            font-size :10px
+            color:rgb(147,153,159)
+          .text
+            line-height :16px
+            font-size :12px
+            color:rgb(7,17,27)
+            .icon-thumb_up,.icon-thumb_down
+              margin-right :4px
+              line-height :24px
+              font-size :12px
+            .icon-thumb_up
+              color:rgb(0,160,220)
+            .icon-thumb_down
+              color:rgb(147,153,159)
+        .no-rating
+          padding :16px 0
+          font-size :12px
+          color:rgb(147,153,159)
+  .slide-enter-active
+    transition: all .5s ease
+    transform:translate3d(0,0,0)
+
+  .slide-leave-active
+    transition: all .5s ease
+    transform:translate3d(0,0,0)
+
+  .slide-enter,.slide-leave-to
+    transform:translate3d(100%,0,0)
+
+  .fade-enter-active,.fade-leave-active
+    opacity :1
+    transition:all 0.4s
+  .fade-enter,.fade-leave-to
+    opacity:0
 </style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,22 +1,167 @@
 <template>
   <div class="goods">
-    <span class="icon" :class="classMap[2]"></span>
+    <div class="menu-wrapper" ref='menuWrapper'>
+      <ul>
+        <li v-for="(item, index) in goods" :key="item.id" class="menu-item" :class="{'current': currentIndex==index}
+        " @click="selectMenu(index, $event)">
+          <span class="text border-1px">
+            <span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper" ref="foodsWrapper">
+      <ul>
+        <li v-for="item in goods" :key='item.id' class="food-list food-list-hook">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" :key='food.id' class="food-item border-1px" @click="selectFood(food, $event)">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon" alt="">
+              </div>
+              <div class="content">
+                <h2 class="name"><span>{{food.name}}</span></h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">
+                    <span class="money-icon">￥</span>
+                    {{food.price}}
+                  </span>
+                  <span class="old" v-show="food.oldPrice">原件{{food.oldPrice}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food" @cartAdd='addToCart'></cartcontrol>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <food :food="selectedFood" ref="food"></food>
   </div>
 </template>
 
 <script>
-export default {
+import cartcontrol from "../cartcontrol/cartcontrol.vue";
+import Bscroll from 'better-scroll';
+import food from "../food/food.vue"
 
+const ERR_OK=0;
+export default {
+  components: {
+    cartcontrol,
+    food,
+  },
+  data() {
+    return {
+      goods: {},
+      listHeight: [],
+      scrollY: 0,
+      selectedFood: {}
+    }
+  },
+  props: {
+    seller: {}
+  },
   created() {
-    this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"]
+    this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
+    this.$http.get('/api/goods').then((res) => {
+      if (res.data.errno === ERR_OK) {
+        this.goods = res.data.data
+        this.$nextTick(()=>{
+          this._initScroll();
+          this.calculateHeight();
+        });
+      }
+    })
+  },
+  methods: {
+    _initScroll(){
+      this.menuScroll = new Bscroll(this.$refs.menuWrapper,{
+        click:true
+      });
+      this.foodsScroll = new Bscroll(this.$refs.foodsWrapper,{
+        probeType:3,
+        click:true
+      });
+      this.foodsScroll.on("scroll",(pos)=>{
+        this.scrollY = Math.abs(Math.round(pos.y));
+      });
+    },        
+
+    selectMenu(index, event) {
+      if (!event._constructed) {
+        return
+      }
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let el = foodList[index]
+      this.foodsScroll.scrollToElement(el, 300)
+    },
+
+    calculateHeight() {
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let height = 0;
+      this.listHeight.push(height);
+      for (let index = 0; index < foodList.length; index++) {
+        const element = foodList[index];
+        height += element.clientHeight;
+        this.listHeight.push(height)
+      }
+    },
+    addToCart() {
+      
+    },
+    selectFood(food, event) {
+      if (!event._constructed) {
+        return
+      }
+      console.log(this.$refs)
+      this.selectedFood = food;
+      this.$refs.food.show()
+    },
+    
+  },
+  computed: {
+    currentIndex() {
+      for (let index = 0; index < this.listHeight.length; index++) {
+        let height1 = this.listHeight[index];
+        let height2 = this.listHeight[index+1];
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return index
+        }
+      }
+      return 0
+    }
   }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
 @import '../../common/stylus/mixin.styl';
-
-    .icon
+  .goods
+    display :flex
+    overflow :hidden
+    position :absolute
+    top: 174px
+    bottom :46px
+    width: 100%
+    .menu-wrapper
+      flex:0 0 80px
+      width :80px
+      background :#f3f5f7
+      .menu-item
+        display :table
+        height :54px
+        width: 56px
+        padding:0 12px
+        line-height :14px
+        .icon
           display: inline-block
           vertical-align: top
           width :12px
@@ -32,6 +177,81 @@ export default {
             bg-image("guarantee_3")
           &.invoice
             bg-image("invoice_3")
-          &.special  
+          &.special
             bg-image("special_3")
+        .text
+          display: table-cell
+          width :56px
+          vertical-align: middle
+          font-size :12px
+          border-1px(rgba(7,17,27,0.1))
+        &.current
+          position :relative
+          margin-top :-1px
+          z-index :10
+          font-weight :700
+          background :#ffffff
+          .text
+            border-none()
+    .foods-wrapper
+      flex:1
+      .title
+        padding-left :14px
+        height: 26px
+        line-height :26px
+        border-left :2px solid #d9dde1
+        font-size :12px
+        color:rgb(147,153,159)
+        background :#f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom :18px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border:none
+          margin-bottom :0
+        .icon
+          flex:0 0 57px
+          margin-right :10px
+          border-radius :2px
+        .content
+          flex:1
+          .name
+            margin:2px 0 8px 0
+            height :14px
+            line-height :14px
+            font-size :14px
+            color :rgb(7,17,27)
+          .desc,.extra
+
+            line-height :10px
+            font-size :10px
+            color :rgb(147,153,159)
+          .desc
+            margin-bottom :8px
+            line-height :14px
+          .extra
+            &.count
+              margin-right :12px
+          .price
+            line-height :24px
+            .now
+              margin-right: 18px
+              font-size :14px
+              font-weight :700
+              color:rgb(240,20,20)
+              .money-icon
+                font-size :10px
+            .old
+              text-decoration :line-through
+              font-size :10px
+              font-weight :700
+              margin-left -15px
+              color :rgb(147,153,159)
+
+          .cartcontrol-wrapper
+            position :absolute
+            right:0
+            bottom :12px
 </style>
